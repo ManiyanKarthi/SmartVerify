3fb6c440587e412df898fb2164b4801131269838
Lingayian, Basker <Basker.Lingayian@tesco.com>
2019-06-24 18:06:41 +0530

    Smart verification implemented successfully

--- src/views/UserProfile/UserProfile.jsx
+++ src/views/UserProfile/UserProfile.jsx
@@ -18,14 +18,12 @@ class UserProfile extends React.Component {
  
 	constructor(props){
 		super(props);
-		this.state = {billNO: "", billAmount:"", errorMessage:"", successMessage:"", employeeID:"", enableDetails: false, showLoaderImage: false,
-					showSuccessMessage: false, showErrorMessage: false, homePageShowFlag: true, invoiceDataStatus: 10};
+		this.state = {billNO: "", billAmount:"", errorMessage:"", successMessage:"", employeeID:"", enableDetails: false, showLoaderImage: false, predictionData: {},
+					showSuccessMessage: false, showErrorMessage: false, homePageShowFlag: true, invoiceDataStatus: 10,
+					billType: 0 , billDate:'', billAmount:'', billStatus:'', automlPrediction:0, manualPrediction:0, billImage:''};
 		this.state.tableColumns = [{"title":"Employee ID","field":"employee_no","type":"numeric"}, {"title":"Month","field":"bill_month","type":"numeric"},{"title":"Bill Submitted","field":"submitted","type":"numeric"},{"title":"Bill Rejected","field":"rejected","type":"numeric"}];
 		this.state.tableDetailedColumns = [{"title":"Bill No","field":"employee_no","type":"numeric"},{"title":"Date","field":"bill_date","type":"date"},{"title":"Amount","field":"bill_amount","type":"numeric"},{"title":"status","field":"bill_status","type":"numeric"}];
-		this.state.billData = {billType:'', billDate:'', billAmount:'', billStatus:'', automlPrediction:'', manualPrediction:'', billImage:''};
 		this.state.billTypeList = [ "Others", "Fuel", "Toll"];
-		this.state.billType = 0;
-		this.state.billDate = this.getCurrentDate();
 		this.state.monthData = this.generateBillMonth();
 		this.state.billMonth = this.state.monthData.monthList[0].value;
 		this.onRowClick = this.onRowClick.bind(this);
@@ -103,9 +101,12 @@ class UserProfile extends React.Component {
 		let url = "/invoice/smart-verify/" + this.state.billID;
 		this.setState({"showLoaderImage": true});
 		new FetchApi().smartVerifyStoredBill({body: {}, url:url, success: function(data){
-			if(data.message === "Success"){
-				ToastsStore.success("Smart verified successfully");
+			if(data.status === 200){
+				ToastsStore.success("Smart verified successfully: " + data.msg);
 				_this.onDetailedRowClick({}, _this.billDetailedClickedData);
+			}
+			else {
+				ToastsStore.error("Smart verified failed: " + data.msg);
 			}
 			_this.setState({"showLoaderImage": false});
 		}, onTimeout: function(){
@@ -190,9 +191,17 @@ class UserProfile extends React.Component {
 		this.billDetailedClickedData = data;
 		new FetchApi().getInvoiceDetails({body: {}, url: url, success: function(res){
 			let invoiceData = res.invoice_list[0];
+			let predictionData = {};
+			if(invoiceData.bill_transaforms){
+				predictionData.billDate = invoiceData.bill_transaforms.bill_date;
+				predictionData.billAmount = invoiceData.bill_transaforms.net_sales;
+				if(typeof invoiceData.bill_transaforms.bill_type === "number"){
+					predictionData.billType = _this.state.billTypeList[invoiceData.bill_transaforms.bill_type];
+				}
+			}
 			_this.setState({"enableDetails":true, billType: invoiceData.bill_type, billDate: invoiceData.bill_date, billAmount: invoiceData.bill_amount, billStatus: invoiceData.bill_status, 
-			automlPrediction: invoiceData.prediction, manualPrediction: invoiceData.manualprediction, invoiceDataStatus: invoiceData.verify_status, billID: invoiceData["_id"], 
-			billNo: invoiceData.bill_no, billImage: new FetchApi().appendURL("/"+invoiceData.invoice_image_loc)}, function(){
+			automlPrediction: invoiceData.automl_prediction_percentage, manualPrediction: invoiceData.automl_prediction_percentage, invoiceDataStatus: invoiceData.verify_status, billID: invoiceData["_id"], 
+			predictionData: predictionData, billNo: invoiceData.bill_no, billImage: new FetchApi().appendURL("/"+invoiceData.invoice_image_loc)}, function(){
 				_this.forceUpdate();
 			});
 		}});
@@ -298,6 +307,13 @@ class UserProfile extends React.Component {
 													})
 												}
 											</Select>
+											{
+												this.state.predictionData.billType ? 
+												<label className={"predictionDataLabel"}>
+													({this.state.predictionData.billType})
+												</label>
+												: null
+											}
 										</Grid>
 									</Grid>
 									<Grid container className={"gridSpaceForm"}>
@@ -307,6 +323,13 @@ class UserProfile extends React.Component {
 										<Grid item xs={4}>
 											<TextField value={this.state.billDate} type={"date"} disabled={disableEdit}
 												onChange={(e) => {this.setState({"billDate": e.currentTarget.value})}}/>
+											{
+												this.state.predictionData.billDate ? 
+												<label className={"predictionDataLabel"}>
+													({this.state.predictionData.billDate})
+												</label>
+												: null
+											}
 										</Grid>
 									</Grid>
 									<Grid container className={"gridSpaceForm"}>
@@ -316,6 +339,13 @@ class UserProfile extends React.Component {
 										<Grid item xs={4}>
 											<TextField type="number" value={this.state.billAmount} disabled={disableEdit}
 												onChange={(e) => {this.setState({"billAmount": e.currentTarget.value})}}/>
+											{
+												this.state.predictionData.billAmount ? 
+												<label className={"predictionDataLabel"}>
+													({this.state.predictionData.billAmount})
+												</label>
+												: null
+											}
 										</Grid>
 									</Grid>
 									<Grid container className={"gridSpaceForm"}>
@@ -331,7 +361,7 @@ class UserProfile extends React.Component {
 											<label className="popupLabelText">AutoML Prediction:</label>
 										</Grid>
 										<Grid item xs={4}>
-											<TextField type="number" value={this.state.automlPrediction}  disabled={disableEdit} 
+											<TextField type="number" value={this.state.automlPrediction}  disabled={true} 
 												onChange={(e) => {this.setState({"automlPrediction": e.currentTarget.value})}}/>
 										</Grid>
 									</Grid>
